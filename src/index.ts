0be345a61df92b0eb26a6d1f08b63dba4ce1d6ab
jihun04 <yujihun20060217@gmail.com>
2022-02-04 14:24:34 +0900

    Classes on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-const name = "jihun",
-    age = 17,
-    gender = "male";
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender?: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
 
+const person = new Human("jihun", 17, "male");
 
-const sayHi = (name: string, age: number, gender: string): void => {
-    console.log(`Hi ${name}, you are ${age}, you are a ${gender}`);
+const sayHi = (person: Human): void => {
+    console.log(`Hi ${person.name}, you are ${person.age}, you are a ${person.gender}`);
 };
 
-sayHi(name, age, gender);
+sayHi(person);
 
 export { };
