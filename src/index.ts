d9f431971353e90cd9c40ca6cfe1673eb7bcb0e7
jihun04 <yujihun20060217@gmail.com>
2022-02-04 14:42:09 +0900

    Creating a Block and Blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age: number, gender?: string) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const person = new Human("jihun", 17, "male");
+const genesisBlock: Block = new Block(0, "345", "", "Hello", 12345);
 
-const sayHi = (person: Human): void => {
-    console.log(`Hi ${person.name}, you are ${person.age}, you are a ${person.gender}`);
-};
+let blockchain: [Block] = [genesisBlock];
 
-sayHi(person);
+console.log(blockchain);
 
 export { };
